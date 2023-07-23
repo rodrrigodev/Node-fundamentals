f7365f83844db6736268cb437f0fdf1728f76b14
Rodrigo <rodrigosanyy@gmail.com>
2023-07-23 19:34:45 -0300

    headers

--- src/server.js
+++ src/server.js
@@ -23,4 +23,6 @@ const server = http.createServer((req, res)=>{
     return res.end('Hello World!!')
 })
 
-server.listen(3333)
+server.listen(3333, ()=> {
+    console.log('Server is running on http://localhost:3333');
+})
