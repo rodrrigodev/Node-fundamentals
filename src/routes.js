6e2fae5e28901a7a7e275886fa2666b0029057f1
Rodrigo <rodrigosanyy@gmail.com>
2023-08-12 09:22:00 -0300

    remove itens

--- src/routes.js
+++ src/routes.js
@@ -34,8 +34,12 @@ export const routes = [
     {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
-        handler: (req, res)=>{    
-            return res.end()
+        handler: (req, res)=>{   
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         }
     }
 ]
